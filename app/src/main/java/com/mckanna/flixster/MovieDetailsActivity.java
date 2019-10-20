dc079fb0ddc56075d01d5b7b2627afcd6c95b9f1
thomas <thomasmckanna@gmail.com>
2019-10-20 18:03:18 -0500

    MovieDetailsActivity deserializes the movie and stores it.

--- app/src/main/java/com/mckanna/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/mckanna/flixster/MovieDetailsActivity.java
@@ -2,13 +2,28 @@ package com.mckanna.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+
+import com.mckanna.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+
+    private Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        // Unwrap the movie passed in via intent
+        Parcelable parcel = getIntent().getParcelableExtra(Movie.class.getSimpleName());
+        movie = (Movie) Parcels.unwrap(parcel);
+        Log.d(getClass().getSimpleName(), String.format("Showing details for '%s'", movie.getTitle()));
+
     }
 }
