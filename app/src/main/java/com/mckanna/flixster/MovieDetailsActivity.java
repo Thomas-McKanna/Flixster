fd9908769c6a7a39817290ef989e409ff3e66a38
thomas <thomasmckanna@gmail.com>
2019-10-20 22:05:15 -0500

    Defined OnClickListener for ImageView in MovieDetailActivity.

--- app/src/main/java/com/mckanna/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/mckanna/flixster/MovieDetailsActivity.java
@@ -2,13 +2,18 @@ package com.mckanna.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.mckanna.flixster.models.Movie;
 
 import org.parceler.Parcels;
@@ -17,6 +22,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
     private Movie movie;
 
+    private ImageView ivBackdrop;
     private TextView tvTitle;
     private TextView tvOverview;
     private RatingBar rbVoteAverage;
@@ -30,6 +36,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivBackdrop = (ImageView) findViewById(R.id.ivBackdrop);
 
         // Unwrap the movie passed in via intent
         Parcelable parcel = getIntent().getParcelableExtra(Movie.class.getSimpleName());
@@ -42,5 +49,25 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
         float voteAverage = movie.getVoteAverage().floatValue() / 2.0f;
         rbVoteAverage.setRating(voteAverage);
+
+        // Set the backdrop
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.placeholder(R.drawable.popcorn);
+
+        Glide.with(this)
+                // Shows a loading gif while the image loads
+                .setDefaultRequestOptions(requestOptions)
+                .load(movie.getBackdropPath())
+                .into(ivBackdrop);
+
+        // Set OnClickListener for ImageView
+        ivBackdrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MovieDetailsActivity.this, MovieTrailerActivity.class);
+
+                MovieDetailsActivity.this.startActivity(intent);
+            }
+        });
     }
 }
